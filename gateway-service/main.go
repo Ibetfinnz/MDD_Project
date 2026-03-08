65d2f86b6c1243db099ddead10ac00216d1aa95a
OlDeuS <66070083@kmitl.ac.th>
2026-03-08 13:22:39 +0700

    Adjust circuit breaker settings to require more requests and lower failure ratio for trip

--- gateway-service/main.go
+++ gateway-service/main.go
@@ -91,7 +91,7 @@ func newCircuitBreaker(name string) *gobreaker.CircuitBreaker {
 		Timeout:     10 * time.Second,
 		ReadyToTrip: func(counts gobreaker.Counts) bool {
 			failureRatio := float64(counts.TotalFailures) / float64(counts.Requests)
-			return counts.Requests >= 2 && failureRatio >= 1.0
+			return counts.Requests >= 3 && failureRatio >= 0.5
 		},
 	}
 	return gobreaker.NewCircuitBreaker(settings)
